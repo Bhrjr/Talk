fccfa99e6fc6efb51f5b3079d27a2e86fe27ec32
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-29 05:18:14 +0900

    Update Base_152.rb

--- 00_System/Base_152.rb
+++ 00_System/Base_152.rb
@@ -20,31 +20,31 @@ class MsgTamamo_A < MsgBase
     unless $game_party.in_partybattle? or $msg.t_enemy.love > 0
       #▼会話対象が主人公の場合
       if $msg.t_target == $game_actors[101]
-        @target = "そなた" #主人公への二人称
+        @target = "boy" #主人公への二人称
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
-          @partner = "そなた" #仲間への二人称
-          @partner = "童" if $msg.age2 > 0 #年下仲間への二人称
-          @partner = "そなた" if $msg.age2 < 0 #年上仲間への二人称
-          @partner = "おぬし" if @doppel_pt #同属仲間への二人称
+          @partner = "you" #仲間への二人称
+          @partner = "child" if $msg.age2 > 0 #年下仲間への二人称
+          @partner = "you" if $msg.age2 < 0 #年上仲間への二人称
+          @partner = "girl" if @doppel_pt #同属仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
-        @target = "そなた" #仲間への二人称
-        @target = "童" if $msg.age > 0 #年下仲間への二人称
-        @target = "そなた" if $msg.age < 0 #年上仲間への二人称
-        @target = "おぬし" if @doppel #同属仲間への二人称
+        @target = "you" #仲間への二人称
+        @target = "child" if $msg.age > 0 #年下仲間への二人称
+        @target = "you" if $msg.age < 0 #年上仲間への二人称
+        @target = "girl" if @doppel #同属仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
-        @partner = "そなた" #主人公への二人称
+        @partner = "boy" #主人公への二人称
       end
       #▼連携が発生している場合、別途呼称を設定する
       if $game_switches[97] == true
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
-          @cp_leader = "そなた" #相方への二人称
-          @cp_leader = "童" if $msg.age3 > 0 #年下相方への二人称
-          @cp_leader = "そなた" if $msg.age3 < 0 #年上相方への二人称
-          @cp_leader = "おぬし" if @doppel_cp #同属相方への二人称
+          @cp_leader = "you" #相方への二人称
+          @cp_leader = "child" if $msg.age3 > 0 #年下相方への二人称
+          @cp_leader = "you" if $msg.age3 < 0 #年上相方への二人称
+          @cp_leader = "girl" if @doppel_cp #同属相方への二人称
         end
       end
     #=====================================================================
@@ -57,30 +57,30 @@ class MsgTamamo_A < MsgBase
           if @companion_pt == true#相手の事を知っている場合の二人称
             @partner = "#{$msg.short_name($msg.t_partner)}" #仲間への二人称
             @partner = "#{$msg.short_name($msg.t_partner)}" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}どの" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}どの" if @doppel_pt #同属仲間への二人称
+            @partner = "Miss #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "Miss #{$msg.short_name($msg.t_partner)}" if @doppel_pt #同属仲間への二人称
           else
-            @partner = "そなた" #仲間への二人称
-            @partner = "童" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "そなた" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "おぬし" if @doppel_pt #同属仲間への二人称
+            @partner = "you" #仲間への二人称
+            @partner = "child" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "you" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "girl" if @doppel_pt #同属仲間への二人称
           end
-          @partner = "奥方" if $msg.t_partner.have_ability?("大切な人") #対象が【大切な人】持ちの場合特殊呼称になる
+          @partner = "love" if $msg.t_partner.have_ability?("大切な人") #対象が【大切な人】持ちの場合特殊呼称になる
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
         if @companion == true#相手の事を知っている場合の二人称
           @target = "#{$msg.short_name($msg.t_target)}" #仲間への二人称
           @target = "#{$msg.short_name($msg.t_target)}" if $msg.age > 0 #年下仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}どの" if $msg.age < 0 #年上仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}どの" if @doppel #同属仲間への二人称
+          @target = "Miss #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
+          @target = "Miss #{$msg.short_name($msg.t_target)}" if @doppel #同属仲間への二人称
         else
-          @target = "そなた" #仲間への二人称
-          @target = "童" if $msg.age > 0 #年下仲間への二人称
-          @target = "そなた" if $msg.age < 0 #年上仲間への二人称
-          @target = "おぬし" if @doppel #同属仲間への二人称
+          @target = "you" #仲間への二人称
+          @target = "child" if $msg.age > 0 #年下仲間への二人称
+          @target = "you" if $msg.age < 0 #年上仲間への二人称
+          @target = "girl" if @doppel #同属仲間への二人称
         end
-        @target = "奥方" if $msg.t_target.have_ability?("大切な人") #対象が【大切な人】持ちの場合特殊呼称になる
+        @target = "love" if $msg.t_target.have_ability?("大切な人") #対象が【大切な人】持ちの場合特殊呼称になる
       end
       #▼連携が発生している場合、別途呼称を設定する
       if $game_switches[97] == true
@@ -89,15 +89,15 @@ class MsgTamamo_A < MsgBase
           if @companion_cp == true
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}" #相方への二人称
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if $msg.age3 > 0 #年下相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}どの" if $msg.age3 < 0 #年上相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}どの" if @doppel_cp #同属相方への二人称
+            @cp_leader = "Miss #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
+            @cp_leader = "Miss #{$msg.short_name($msg.coop_leader)}" if @doppel_cp #同属相方への二人称
           else
-            @cp_leader = "そなた" #相方への二人称
-            @cp_leader = "童" if $msg.age3 > 0 #年下仲間への二人称
-            @cp_leader = "そなた" if $msg.age3 < 0 #年上仲間への二人称
-            @cp_leader = "おぬし" if @doppel_cp #同属仲間への二人称
+            @cp_leader = "you" #相方への二人称
+            @cp_leader = "child" if $msg.age3 > 0 #年下仲間への二人称
+            @cp_leader = "you" if $msg.age3 < 0 #年上仲間への二人称
+            @cp_leader = "girl" if @doppel_cp #同属仲間への二人称
           end
-          @cp_leader = "奥方" if $msg.coop_leader.have_ability?("大切な人") #対象が【大切な人】持ちの場合特殊呼称になる
+          @cp_leader = "love" if $msg.coop_leader.have_ability?("大切な人") #対象が【大切な人】持ちの場合特殊呼称になる
         end
       end
     end
