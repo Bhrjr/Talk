88990241285b78885b86c6f4b91980ed34bee3c3
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-29 07:25:07 +0900

    Update Base_74.rb

--- 00_System/Base_74.rb
+++ 00_System/Base_74.rb
@@ -20,31 +20,31 @@ class MsgNightmare_A < MsgBase
     unless $game_party.in_partybattle? or $msg.t_enemy.love > 0
       #▼会話対象が主人公の場合
       if $msg.t_target == $game_actors[101]
-        @target = "おにーさん" #主人公への二人称
+        @target = "Big Bro" #主人公への二人称
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
-          @partner = "あなた" #仲間への二人称
-          @partner = "あなた" if $msg.age2 > 0 #年下仲間への二人称
-          @partner = "おねーさん" if $msg.age2 < 0 #年上仲間への二人称
-          @partner = "あなた" if @doppel_pt #同属仲間への二人称
+          @partner = "Sister" #仲間への二人称
+          @partner = "Sister" if $msg.age2 > 0 #年下仲間への二人称
+          @partner = "Big sister" if $msg.age2 < 0 #年上仲間への二人称
+          @partner = "Sister" if @doppel_pt #同属仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
-        @target = "あなた" #仲間への二人称
-        @target = "あなた" if $msg.age > 0 #年下仲間への二人称
-        @target = "おねーさん" if $msg.age < 0 #年上仲間への二人称
-        @target = "あなた" if @doppel #同属仲間への二人称
+        @target = "Sister" #仲間への二人称
+        @target = "Sister" if $msg.age > 0 #年下仲間への二人称
+        @target = "Big sister" if $msg.age < 0 #年上仲間への二人称
+        @target = "Sister" if @doppel #同属仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
-        @partner = "おにーさん" #主人公への二人称
+        @partner = "Big Bro" #主人公への二人称
       end
       #▼連携が発生している場合、別途呼称を設定する
       if $game_switches[97] == true
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
-          @cp_leader = "あなた" #相方への二人称
-          @cp_leader = "あなた" if $msg.age3 > 0 #年下相方への二人称
-          @cp_leader = "おねーさん" if $msg.age3 < 0 #年上相方への二人称
-          @cp_leader = "あなた" if @doppel_cp #同属相方への二人称
+          @cp_leader = "Sister" #相方への二人称
+          @cp_leader = "Sister" if $msg.age3 > 0 #年下相方への二人称
+          @cp_leader = "Big sister" if $msg.age3 < 0 #年上相方への二人称
+          @cp_leader = "Sister" if @doppel_cp #同属相方への二人称
         end
       end
     #=====================================================================
@@ -56,28 +56,28 @@ class MsgNightmare_A < MsgBase
         if $msg.t_partner != nil
           if @companion_pt == true#相手の事を知っている場合の二人称
             @partner = "#{$msg.short_name($msg.t_partner)}" #仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}ちゃん" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}さん" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "#{$msg.short_name($msg.t_partner)}" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Ms. #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
             @partner = "#{$msg.short_name($msg.t_partner)}" if @doppel_pt #同属仲間への二人称
           else
-            @partner = "あなた" #仲間への二人称
-            @partner = "あなた" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "おねーさん" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "あなた" if @doppel_pt #同属仲間への二人称
+            @partner = "Sister" #仲間への二人称
+            @partner = "Sister" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Big sister" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "Sister" if @doppel_pt #同属仲間への二人称
           end
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
         if @companion == true#相手の事を知っている場合の二人称
           @target = "#{$msg.short_name($msg.t_target)}" #仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}ちゃん" if $msg.age > 0 #年下仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}さん" if $msg.age < 0 #年上仲間への二人称
+          @target = "#{$msg.short_name($msg.t_target)}" if $msg.age > 0 #年下仲間への二人称
+          @target = "Ms. #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
           @target = "#{$msg.short_name($msg.t_target)}" if @doppel #同属仲間への二人称
         else
-          @target = "あなた" #仲間への二人称
-          @target = "あなた" if $msg.age > 0 #年下仲間への二人称
-          @target = "おねーさん" if $msg.age < 0 #年上仲間への二人称
-          @target = "あなた" if @doppel #同属仲間への二人称
+          @target = "Sister" #仲間への二人称
+          @target = "Sister" if $msg.age > 0 #年下仲間への二人称
+          @target = "Big sister" if $msg.age < 0 #年上仲間への二人称
+          @target = "Sister" if @doppel #同属仲間への二人称
         end
       end
       #▼連携が発生している場合、別途呼称を設定する
@@ -86,14 +86,14 @@ class MsgNightmare_A < MsgBase
         if $msg.coop_leader != $msg.t_enemy
           if @companion_cp == true
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}" #相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}ちゃん" if $msg.age3 > 0 #年下相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}さん" if $msg.age3 < 0 #年上相方への二人称
+            @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if $msg.age3 > 0 #年下相方への二人称
+            @cp_leader = "Ms. #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if @doppel_cp #同属相方への二人称
           else
-            @cp_leader = "あなた" #相方への二人称
-            @cp_leader = "あなた" if $msg.age3 > 0 #年下仲間への二人称
-            @cp_leader = "おねーさん" if $msg.age3 < 0 #年上仲間への二人称
-            @cp_leader = "あなた" if @doppel_cp #同属仲間への二人称
+            @cp_leader = "Sister" #相方への二人称
+            @cp_leader = "Sister" if $msg.age3 > 0 #年下仲間への二人称
+            @cp_leader = "Big sister" if $msg.age3 < 0 #年上仲間への二人称
+            @cp_leader = "Sister" if @doppel_cp #同属仲間への二人称
           end
         end
       end
