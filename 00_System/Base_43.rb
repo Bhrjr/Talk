d1295e108eba200e7b4811fc5bdb42ca95f07ed6
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-29 07:11:44 +0900

    Update Base_43.rb

--- 00_System/Base_43.rb
+++ 00_System/Base_43.rb
@@ -20,31 +20,31 @@ class MsgWitch_B < MsgBase
     unless $game_party.in_partybattle? or $msg.t_enemy.love > 0
       #▼会話対象が主人公の場合
       if $msg.t_target == $game_actors[101]
-        @target = "君" #主人公への二人称
+        @target = "bud" #主人公への二人称
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
-          @partner = "貴女" #仲間への二人称
-          @partner = "貴女" if $msg.age2 > 0 #年下仲間への二人称
-          @partner = "貴女" if $msg.age2 < 0 #年上仲間への二人称
-          @partner = "貴女" if @doppel_pt #同属仲間への二人称
+          @partner = "miss" #仲間への二人称
+          @partner = "young miss" if $msg.age2 > 0 #年下仲間への二人称
+          @partner = "miss" if $msg.age2 < 0 #年上仲間への二人称
+          @partner = "miss" if @doppel_pt #同属仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
-        @target = "貴女" #仲間への二人称
-        @target = "君" if $msg.age > 0 #年下仲間への二人称
-        @target = "貴女" if $msg.age < 0 #年上仲間への二人称
-        @target = "貴女" if @doppel #同属仲間への二人称
+        @target = "miss" #仲間への二人称
+        @target = "bud" if $msg.age > 0 #年下仲間への二人称
+        @target = "miss" if $msg.age < 0 #年上仲間への二人称
+        @target = "miss" if @doppel #同属仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
-        @partner = "君" #主人公への二人称
+        @partner = "bud" #主人公への二人称
       end
       #▼連携が発生している場合、別途呼称を設定する
       if $game_switches[97] == true
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
-          @cp_leader = "貴女" #相方への二人称
-          @cp_leader = "君" if $msg.age3 > 0 #年下相方への二人称
-          @cp_leader = "貴女" if $msg.age3 < 0 #年上相方への二人称
-          @cp_leader = "貴女" if @doppel_cp #同属相方への二人称
+          @cp_leader = "miss" #相方への二人称
+          @cp_leader = "bud" if $msg.age3 > 0 #年下相方への二人称
+          @cp_leader = "miss" if $msg.age3 < 0 #年上相方への二人称
+          @cp_leader = "miss" if @doppel_cp #同属相方への二人称
         end
       end
     #=====================================================================
@@ -55,29 +55,29 @@ class MsgWitch_B < MsgBase
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
           if @companion_pt == true#相手の事を知っている場合の二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}君" #仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}君" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}殿" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}先生" if @doppel_pt #同属仲間への二人称
+            @partner = "#{$msg.short_name($msg.t_partner)}" #仲間への二人称
+            @partner = "#{$msg.short_name($msg.t_partner)}" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Ms. #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "Ms. #{$msg.short_name($msg.t_partner)}" if @doppel_pt #同属仲間への二人称
           else
-            @partner = "貴女" #仲間への二人称
-            @partner = "君" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "貴女" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "貴女" if @doppel_pt #同属仲間への二人称
+            @partner = "miss" #仲間への二人称
+            @partner = "bud" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "miss" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "miss" if @doppel_pt #同属仲間への二人称
           end
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
         if @companion == true#相手の事を知っている場合の二人称
-          @target = "#{$msg.short_name($msg.t_target)}君" #仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}君" if $msg.age > 0 #年下仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}殿" if $msg.age < 0 #年上仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}先生" if @doppel #同属仲間への二人称
+          @target = "#{$msg.short_name($msg.t_target)}" #仲間への二人称
+          @target = "#{$msg.short_name($msg.t_target)}" if $msg.age > 0 #年下仲間への二人称
+          @target = "Ms. #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
+          @target = "Ms. #{$msg.short_name($msg.t_target)}" if @doppel #同属仲間への二人称
         else
-          @target = "貴女" #仲間への二人称
-          @target = "君" if $msg.age > 0 #年下仲間への二人称
-          @target = "貴女" if $msg.age < 0 #年上仲間への二人称
-          @target = "貴女" if @doppel #同属仲間への二人称
+          @target = "miss" #仲間への二人称
+          @target = "bud" if $msg.age > 0 #年下仲間への二人称
+          @target = "miss" if $msg.age < 0 #年上仲間への二人称
+          @target = "miss" if @doppel #同属仲間への二人称
         end
       end
       #▼連携が発生している場合、別途呼称を設定する
@@ -85,15 +85,15 @@ class MsgWitch_B < MsgBase
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
           if @companion_cp == true
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}君" #相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}君" if $msg.age3 > 0 #年下相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}殿" if $msg.age3 < 0 #年上相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}先生" if @doppel_cp #同属相方への二人称
+            @cp_leader = "#{$msg.short_name($msg.coop_leader)}" #相方への二人称
+            @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if $msg.age3 > 0 #年下相方への二人称
+            @cp_leader = "Ms. #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
+            @cp_leader = "Ms. #{$msg.short_name($msg.coop_leader)}" if @doppel_cp #同属相方への二人称
           else
-            @cp_leader = "貴女" #相方への二人称
-            @cp_leader = "君" if $msg.age3 > 0 #年下仲間への二人称
-            @cp_leader = "貴女" if $msg.age3 < 0 #年上仲間への二人称
-            @cp_leader = "貴女" if @doppel_cp #同属仲間への二人称
+            @cp_leader = "miss" #相方への二人称
+            @cp_leader = "bud" if $msg.age3 > 0 #年下仲間への二人称
+            @cp_leader = "miss" if $msg.age3 < 0 #年上仲間への二人称
+            @cp_leader = "miss" if @doppel_cp #同属仲間への二人称
           end
         end
       end
