424cc4e076709194195997f590b559c4ac98421c
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-29 07:22:18 +0900

    Update Base_63.rb

--- 00_System/Base_63.rb
+++ 00_System/Base_63.rb
@@ -20,31 +20,31 @@ class MsgSlave_A < MsgBase
     unless $game_party.in_partybattle? or $msg.t_enemy.love > 0
       #▼会話対象が主人公の場合
       if $msg.t_target == $game_actors[101]
-        @target = "ご主人様" #主人公への二人称
+        @target = "Master" #主人公への二人称
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
-          @partner = "ご主人様" #仲間への二人称
-          @partner = "ご主人様" if $msg.age2 > 0 #年下仲間への二人称
-          @partner = "ご主人様" if $msg.age2 < 0 #年上仲間への二人称
-          @partner = "あなた" if @doppel_pt #同属仲間への二人称
+          @partner = "Master" #仲間への二人称
+          @partner = "Young Master" if $msg.age2 > 0 #年下仲間への二人称
+          @partner = "Master" if $msg.age2 < 0 #年上仲間への二人称
+          @partner = "lady" if @doppel_pt #同属仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
-        @target = "ご主人様" #仲間への二人称
-        @target = "ご主人様" if $msg.age > 0 #年下仲間への二人称
-        @target = "ご主人様" if $msg.age < 0 #年上仲間への二人称
-        @target = "あなた" if @doppel #同属仲間への二人称
+        @target = "Master" #仲間への二人称
+        @target = "Young Master" if $msg.age > 0 #年下仲間への二人称
+        @target = "Master" if $msg.age < 0 #年上仲間への二人称
+        @target = "lady" if @doppel #同属仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
-        @partner = "ご主人様" #主人公への二人称
+        @partner = "Master" #主人公への二人称
       end
       #▼連携が発生している場合、別途呼称を設定する
       if $game_switches[97] == true
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
-          @cp_leader = "ご主人様" #相方への二人称
-          @cp_leader = "ご主人様" if $msg.age3 > 0 #年下相方への二人称
-          @cp_leader = "ご主人様" if $msg.age3 < 0 #年上相方への二人称
-          @cp_leader = "あなた" if @doppel_cp #同属相方への二人称
+          @cp_leader = "Master" #相方への二人称
+          @cp_leader = "Young Master" if $msg.age3 > 0 #年下相方への二人称
+          @cp_leader = "Master" if $msg.age3 < 0 #年上相方への二人称
+          @cp_leader = "lady" if @doppel_cp #同属相方への二人称
         end
       end
     #=====================================================================
@@ -55,29 +55,29 @@ class MsgSlave_A < MsgBase
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
           if @companion_pt == true#相手の事を知っている場合の二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}姉様" #仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}ちゃん" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}姉様" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "#{$msg.short_name($msg.t_partner)}ちゃん" if @doppel_pt #同属仲間への二人称
+            @partner = "Sister #{$msg.short_name($msg.t_partner)}" #仲間への二人称
+            @partner = "#{$msg.short_name($msg.t_partner)}" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Sister #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "#{$msg.short_name($msg.t_partner)}" if @doppel_pt #同属仲間への二人称
           else
-            @partner = "姉様" #仲間への二人称
-            @partner = "あなた" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "姉様" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "あなた" if @doppel_pt #同属仲間への二人称
+            @partner = "Sister" #仲間への二人称
+            @partner = "lady" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Big Sister" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "lady" if @doppel_pt #同属仲間への二人称
           end
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
         if @companion == true#相手の事を知っている場合の二人称
-          @target = "#{$msg.short_name($msg.t_target)}姉様" #仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}ちゃん" if $msg.age > 0 #年下仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}姉様" if $msg.age < 0 #年上仲間への二人称
-          @target = "#{$msg.short_name($msg.t_target)}ちゃん" if @doppel #同属仲間への二人称
+          @target = "Sister #{$msg.short_name($msg.t_target)}" #仲間への二人称
+          @target = "#{$msg.short_name($msg.t_target)}" if $msg.age > 0 #年下仲間への二人称
+          @target = "Sister #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
+          @target = "#{$msg.short_name($msg.t_target)}" if @doppel #同属仲間への二人称
         else
-          @target = "姉様" #仲間への二人称
-          @target = "あなた" if $msg.age > 0 #年下仲間への二人称
-          @target = "姉様" if $msg.age < 0 #年上仲間への二人称
-          @target = "あなた" if @doppel #同属仲間への二人称
+          @target = "Sister" #仲間への二人称
+          @target = "lady" if $msg.age > 0 #年下仲間への二人称
+          @target = "Big Sister" if $msg.age < 0 #年上仲間への二人称
+          @target = "lady" if @doppel #同属仲間への二人称
         end
       end
       #▼連携が発生している場合、別途呼称を設定する
@@ -85,15 +85,15 @@ class MsgSlave_A < MsgBase
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
           if @companion_cp == true
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}姉様" #相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}ちゃん" if $msg.age3 > 0 #年下相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}姉様" if $msg.age3 < 0 #年上相方への二人称
-            @cp_leader = "#{$msg.short_name($msg.coop_leader)}ちゃん" if @doppel_cp #同属相方への二人称
+            @cp_leader = "Sister #{$msg.short_name($msg.coop_leader)}" #相方への二人称
+            @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if $msg.age3 > 0 #年下相方への二人称
+            @cp_leader = "Sister #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
+            @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if @doppel_cp #同属相方への二人称
           else
-            @cp_leader = "姉様" #相方への二人称
-            @cp_leader = "あなた" if $msg.age3 > 0 #年下仲間への二人称
-            @cp_leader = "姉様" if $msg.age3 < 0 #年上仲間への二人称
-            @cp_leader = "あなた" if @doppel_cp #同属仲間への二人称
+            @cp_leader = "Sister" #相方への二人称
+            @cp_leader = "lady" if $msg.age3 > 0 #年下仲間への二人称
+            @cp_leader = "Big Sister" if $msg.age3 < 0 #年上仲間への二人称
+            @cp_leader = "lady" if @doppel_cp #同属仲間への二人称
           end
         end
       end
