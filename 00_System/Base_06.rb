89e880a3b1820cd97c0c85c533eb83bbeb6b5bde
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-29 04:02:20 +0900

    Update Base_06.rb

--- 00_System/Base_06.rb
+++ 00_System/Base_06.rb
@@ -23,17 +23,17 @@ class MsgLessersuccubus_B < MsgBase
         @target = "Bro" #主人公への二人称
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
-          @partner = "you" #仲間への二人称
-          @partner = "you" if $msg.age2 > 0 #年下仲間への二人称
-          @partner = "Sis" if $msg.age2 < 0 #年上仲間への二人称
-          @partner = "you" if @doppel_pt #同属仲間への二人称
+          @partner = "Sis" #仲間への二人称
+          @partner = "Sis" if $msg.age2 > 0 #年下仲間への二人称
+          @partner = "Big Sis" if $msg.age2 < 0 #年上仲間への二人称
+          @partner = "Sis" if @doppel_pt #同属仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
-        @target = "you" #仲間への二人称
-        @target = "you" if $msg.age > 0 #年下仲間への二人称
-        @target = "Sis" if $msg.age < 0 #年上仲間への二人称
-        @target = "you" if @doppel #同属仲間への二人称
+        @target = "Sis" #仲間への二人称
+        @target = "Sis" if $msg.age > 0 #年下仲間への二人称
+        @target = "Big Sis" if $msg.age < 0 #年上仲間への二人称
+        @target = "Sis" if @doppel #同属仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
         @partner = "Bro" #主人公への二人称
       end
@@ -41,10 +41,10 @@ class MsgLessersuccubus_B < MsgBase
       if $game_switches[97] == true
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
-          @cp_leader = "you" #相方への二人称
-          @cp_leader = "you" if $msg.age3 > 0 #年下相方への二人称
-          @cp_leader = "Sis" if $msg.age3 < 0 #年上相方への二人称
-          @cp_leader = "you" if @doppel_cp #同属相方への二人称
+          @cp_leader = "Sis" #相方への二人称
+          @cp_leader = "Sis" if $msg.age3 > 0 #年下相方への二人称
+          @cp_leader = "Big Sis" if $msg.age3 < 0 #年上相方への二人称
+          @cp_leader = "Sis" if @doppel_cp #同属相方への二人称
         end
       end
     #=====================================================================
@@ -60,10 +60,10 @@ class MsgLessersuccubus_B < MsgBase
             @partner = "Ms. #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
             @partner = "#{$msg.short_name($msg.t_partner)}" if @doppel_pt #同属仲間への二人称
           else
-            @partner = "you" #仲間への二人称
-            @partner = "you" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "Sis" if $msg.age2 < 0 #年上仲間への二人称
-            @partner = "you" if @doppel_pt #同属仲間への二人称
+            @partner = "Sis" #仲間への二人称
+            @partner = "Sis" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Big Sis" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "Sis" if @doppel_pt #同属仲間への二人称
           end
         end
       #▼会話対象が主人公でない場合
@@ -74,10 +74,10 @@ class MsgLessersuccubus_B < MsgBase
           @target = "Ms. #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
           @target = "#{$msg.short_name($msg.t_target)}" if @doppel #同属仲間への二人称
         else
-          @target = "you" #仲間への二人称
-          @target = "you" if $msg.age > 0 #年下仲間への二人称
-          @target = "Sis" if $msg.age < 0 #年上仲間への二人称
-          @target = "you" if @doppel #同属仲間への二人称
+          @target = "Sis" #仲間への二人称
+          @target = "Sis" if $msg.age > 0 #年下仲間への二人称
+          @target = "Big Sis" if $msg.age < 0 #年上仲間への二人称
+          @target = "Sis" if @doppel #同属仲間への二人称
         end
       end
       #▼連携が発生している場合、別途呼称を設定する
@@ -90,10 +90,10 @@ class MsgLessersuccubus_B < MsgBase
             @cp_leader = "Ms. #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if @doppel_cp #同属相方への二人称
           else
-            @cp_leader = "you" #相方への二人称
-            @cp_leader = "you" if $msg.age3 > 0 #年下仲間への二人称
-            @cp_leader = "Sis" if $msg.age3 < 0 #年上仲間への二人称
-            @cp_leader = "you" if @doppel_cp #同属仲間への二人称
+            @cp_leader = "Sis" #相方への二人称
+            @cp_leader = "Sis" if $msg.age3 > 0 #年下仲間への二人称
+            @cp_leader = "Big Sis" if $msg.age3 < 0 #年上仲間への二人称
+            @cp_leader = "Sis" if @doppel_cp #同属仲間への二人称
           end
         end
       end
