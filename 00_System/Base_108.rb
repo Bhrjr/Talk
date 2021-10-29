6de5a8577c7653b8d62ea0798f019bc85752127e
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-29 11:21:53 +0900

    Update Base_108.rb

--- 00_System/Base_108.rb
+++ 00_System/Base_108.rb
@@ -24,14 +24,14 @@ class MsgGoblin_A < MsgBase
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
           @partner = "Sissy" #仲間への二人称
-          @partner = "Li'l sissy" if $msg.age2 > 0 #年下仲間への二人称
+          @partner = "Li'l girl" if $msg.age2 > 0 #年下仲間への二人称
           @partner = "Big sissy" if $msg.age2 < 0 #年上仲間への二人称
           @partner = "Sissy" if @doppel_pt #同属仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
         @target = "Sissy" #仲間への二人称
-        @target = "Li'l sissy" if $msg.age > 0 #年下仲間への二人称
+        @target = "Li'l girl" if $msg.age > 0 #年下仲間への二人称
         @target = "Big sissy" if $msg.age < 0 #年上仲間への二人称
         @target = "Sissy" if @doppel #同属仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
@@ -42,7 +42,7 @@ class MsgGoblin_A < MsgBase
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
           @cp_leader = "Sissy" #相方への二人称
-          @cp_leader = "Li'l sissy" if $msg.age3 > 0 #年下相方への二人称
+          @cp_leader = "Li'l girl" if $msg.age3 > 0 #年下相方への二人称
           @cp_leader = "Big sissy" if $msg.age3 < 0 #年上相方への二人称
           @cp_leader = "Sissy" if @doppel_cp #同属相方への二人称
           @cp_leader = "Big sister" if $data_SDB[$msg.coop_leader.class_id].name == "Gang Leader" #連携相手がギャングリーダー
@@ -57,13 +57,13 @@ class MsgGoblin_A < MsgBase
         if $msg.t_partner != nil
           if @companion_pt == true#相手の事を知っている場合の二人称
             @partner = "#{$msg.short_name($msg.t_partner)}" #仲間への二人称
-            @partner = "Li'l sis #{$msg.short_name($msg.t_partner)}" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Li'l #{$msg.short_name($msg.t_partner)}" if $msg.age2 > 0 #年下仲間への二人称
             @partner = "Big sis #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
             @partner = "#{$msg.short_name($msg.t_partner)}" if @doppel_pt #同属仲間への二人称
             @partner = "Big sis #{$msg.short_name($msg.t_partner)}" if $data_SDB[$msg.t_partner.class_id].name == "Gang Leader" #相手がギャングリーダー
           else
             @partner = "Sissy" #仲間への二人称
-            @partner = "Li'l sissy" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "Li'l girl" if $msg.age2 > 0 #年下仲間への二人称
             @partner = "Big sissy" if $msg.age2 < 0 #年上仲間への二人称
             @partner = "Sissy" if @doppel_pt #同属仲間への二人称
             @partner = "Big sister" if $data_SDB[$msg.t_partner.class_id].name == "Gang Leader" #相手がギャングリーダー
@@ -73,13 +73,13 @@ class MsgGoblin_A < MsgBase
       elsif $msg.t_target != $game_actors[101]
         if @companion == true#相手の事を知っている場合の二人称
           @target = "#{$msg.short_name($msg.t_target)}" #仲間への二人称
-          @target = "Li'l sis #{$msg.short_name($msg.t_target)}" if $msg.age > 0 #年下仲間への二人称
+          @target = "Li'l #{$msg.short_name($msg.t_target)}" if $msg.age > 0 #年下仲間への二人称
           @target = "Big sis #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
           @target = "#{$msg.short_name($msg.t_target)}" if @doppel #同属仲間への二人称
           @target = "Big sis #{$msg.short_name($msg.t_target)}" if $data_SDB[$msg.t_target.class_id].name == "Gang Leader" #相手がギャングリーダー
         else
           @target = "Sissy" #仲間への二人称
-          @target = "Li'l sissy" if $msg.age > 0 #年下仲間への二人称
+          @target = "Li'l girl" if $msg.age > 0 #年下仲間への二人称
           @target = "Big sissy" if $msg.age < 0 #年上仲間への二人称
           @target = "Sissy" if @doppel #同属仲間への二人称
           @target = "Big sister" if $data_SDB[$msg.t_target.class_id].name == "Gang Leader" #相手がギャングリーダー
@@ -91,13 +91,13 @@ class MsgGoblin_A < MsgBase
         if $msg.coop_leader != $msg.t_enemy
           if @companion_cp == true
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}" #相方への二人称
-            @cp_leader = "Li'l sis #{$msg.short_name($msg.coop_leader)}" if $msg.age3 > 0 #年下相方への二人称
+            @cp_leader = "Li'l #{$msg.short_name($msg.coop_leader)}" if $msg.age3 > 0 #年下相方への二人称
             @cp_leader = "Big sis #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}" if @doppel_cp #同属相方への二人称
             @cp_leader = "Big sis #{$msg.short_name($msg.coop_leader)}" if $data_SDB[$msg.coop_leader.class_id].name == "Gang Leader" #連携相手がギャングリーダー
           else
             @cp_leader = "Sissy" #相方への二人称
-            @cp_leader = "Li'l sissy" if $msg.age3 > 0 #年下仲間への二人称
+            @cp_leader = "Li'l girl" if $msg.age3 > 0 #年下仲間への二人称
             @cp_leader = "Big sissy" if $msg.age3 < 0 #年上仲間への二人称
             @cp_leader = "Sissy" if @doppel_cp #同属仲間への二人称
             @cp_leader = "Big sister" if $data_SDB[$msg.coop_leader.class_id].name == "Gang Leader" #連携相手がギャングリーダー
