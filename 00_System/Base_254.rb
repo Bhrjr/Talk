948a7138d1cf40d03f34af2df9b24ad1e8426707
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-30 00:22:51 +0900

    Update Base_254.rb

--- 00_System/Base_254.rb
+++ 00_System/Base_254.rb
@@ -20,28 +20,28 @@ class MsgGilgoon < MsgBase
     unless $game_party.in_partybattle? or $msg.t_enemy.love > 0
       #▼会話対象が主人公の場合
       if $msg.t_target == $game_actors[101]
-        @target = "you" #主人公への二人称
+        @target = "You" #主人公への二人称
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
-          @partner = "you" #仲間への二人称
-          @partner = "you" if $msg.age2 > 0 #年下仲間への二人称
-          @partner = "you" if $msg.age2 < 0 #年上仲間への二人称
+          @partner = "You" #仲間への二人称
+          @partner = "You" if $msg.age2 > 0 #年下仲間への二人称
+          @partner = "You" if $msg.age2 < 0 #年上仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
-        @target = "you" #仲間への二人称
-        @target = "you" if $msg.age > 0 #年下仲間への二人称
-        @target = "you" if $msg.age < 0 #年上仲間への二人称
+        @target = "You" #仲間への二人称
+        @target = "You" if $msg.age > 0 #年下仲間への二人称
+        @target = "You" if $msg.age < 0 #年上仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
-        @partner = "you" #主人公への二人称
+        @partner = "You" #主人公への二人称
       end
       #▼連携が発生している場合、別途呼称を設定する
       if $game_switches[97] == true
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
-          @cp_leader = "you" #相方への二人称
-          @cp_leader = "you" if $msg.age3 > 0 #年下相方への二人称
-          @cp_leader = "you" if $msg.age3 < 0 #年上相方への二人称
+          @cp_leader = "You" #相方への二人称
+          @cp_leader = "You" if $msg.age3 > 0 #年下相方への二人称
+          @cp_leader = "You" if $msg.age3 < 0 #年上相方への二人称
         end
       end
     #=====================================================================
@@ -52,25 +52,25 @@ class MsgGilgoon < MsgBase
         #パートナーが存在する場合別途設定
         if $msg.t_partner != nil
           if @companion_pt == true#相手の事を知っている場合の二人称
-            @partner = "you" #仲間への二人称
-            @partner = "you" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "you" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "You" #仲間への二人称
+            @partner = "You" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "You" if $msg.age2 < 0 #年上仲間への二人称
           else
-            @partner = "you" #仲間への二人称
-            @partner = "you" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "you" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "You" #仲間への二人称
+            @partner = "You" if $msg.age2 > 0 #年下仲間への二人称
+            @partner = "You" if $msg.age2 < 0 #年上仲間への二人称
           end
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
         if @companion == true#相手の事を知っている場合の二人称
-          @target = "you" #仲間への二人称
-          @target = "you" if $msg.age > 0 #年下仲間への二人称
-          @target = "you" if $msg.age < 0 #年上仲間への二人称
+          @target = "You" #仲間への二人称
+          @target = "You" if $msg.age > 0 #年下仲間への二人称
+          @target = "You" if $msg.age < 0 #年上仲間への二人称
         else
-          @target = "you" #仲間への二人称
-          @target = "you" if $msg.age > 0 #年下仲間への二人称
-          @target = "you" if $msg.age < 0 #年上仲間への二人称
+          @target = "You" #仲間への二人称
+          @target = "You" if $msg.age > 0 #年下仲間への二人称
+          @target = "You" if $msg.age < 0 #年上仲間への二人称
         end
       end
       #▼連携が発生している場合、別途呼称を設定する
@@ -78,13 +78,13 @@ class MsgGilgoon < MsgBase
         #会話の仕掛け手が自分で無い場合、呼称を設定する
         if $msg.coop_leader != $msg.t_enemy
           if @companion_cp == true
-            @cp_leader = "you" #相方への二人称
-            @cp_leader = "you" if $msg.age3 > 0 #年下相方への二人称
-            @cp_leader = "you" if $msg.age3 < 0 #年上相方への二人称
+            @cp_leader = "You" #相方への二人称
+            @cp_leader = "You" if $msg.age3 > 0 #年下相方への二人称
+            @cp_leader = "You" if $msg.age3 < 0 #年上相方への二人称
           else
-            @cp_leader = "you" #相方への二人称
-            @cp_leader = "you" if $msg.age3 > 0 #年下仲間への二人称
-            @cp_leader = "you" if $msg.age3 < 0 #年上仲間への二人称
+            @cp_leader = "You" #相方への二人称
+            @cp_leader = "You" if $msg.age3 > 0 #年下仲間への二人称
+            @cp_leader = "You" if $msg.age3 < 0 #年上仲間への二人称
           end
         end
       end
