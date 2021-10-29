48837b6996e60f5218f4d2b4fabaa2c8b454c586
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-10-29 10:42:27 +0900

    Update Base_111.rb

--- 00_System/Base_111.rb
+++ 00_System/Base_111.rb
@@ -25,14 +25,14 @@ class MsgGangcommander_A < MsgBase
         if $msg.t_partner != nil
           @partner = "Sis" #仲間への二人称
           @partner = "Sis" if $msg.age2 > 0 #年下仲間への二人称
-          @partner = "Big Sis" if $msg.age2 < 0 #年上仲間への二人称
+          @partner = "Big sis" if $msg.age2 < 0 #年上仲間への二人称
           @partner = "Sis" if @doppel_pt #同属仲間への二人称
         end
       #▼会話対象が主人公でない場合
       elsif $msg.t_target != $game_actors[101]
         @target = "Sis" #仲間への二人称
         @target = "Sis" if $msg.age > 0 #年下仲間への二人称
-        @target = "Big Sis" if $msg.age < 0 #年上仲間への二人称
+        @target = "Big sis" if $msg.age < 0 #年上仲間への二人称
         @target = "Sis" if @doppel #同属仲間への二人称
         #会話対象がパートナーなので、相方は自動的に主人公となる
         @partner = "Bro" #主人公への二人称
@@ -43,7 +43,7 @@ class MsgGangcommander_A < MsgBase
         if $msg.coop_leader != $msg.t_enemy
           @cp_leader = "Sis" #相方への二人称
           @cp_leader = "Sis" if $msg.age3 > 0 #年下相方への二人称
-          @cp_leader = "Big Sis" if $msg.age3 < 0 #年上相方への二人称
+          @cp_leader = "Big sis" if $msg.age3 < 0 #年上相方への二人称
           @cp_leader = "Sis" if @doppel_cp #同属相方への二人称
         end
       end
@@ -57,12 +57,12 @@ class MsgGangcommander_A < MsgBase
           if @companion_pt == true#相手の事を知っている場合の二人称
             @partner = "#{$msg.short_name($msg.t_partner)}-girl" #仲間への二人称
             @partner = "#{$msg.short_name($msg.t_partner)}-girl" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "Sister #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "Big sis #{$msg.short_name($msg.t_partner)}" if $msg.age2 < 0 #年上仲間への二人称
             @partner = "#{$msg.short_name($msg.t_partner)}-girl" if @doppel_pt #同属仲間への二人称
           else
             @partner = "Sis" #仲間への二人称
             @partner = "Sis" if $msg.age2 > 0 #年下仲間への二人称
-            @partner = "Big Sister" if $msg.age2 < 0 #年上仲間への二人称
+            @partner = "Big sister" if $msg.age2 < 0 #年上仲間への二人称
             @partner = "Sis" if @doppel_pt #同属仲間への二人称
           end
         end
@@ -71,12 +71,12 @@ class MsgGangcommander_A < MsgBase
         if @companion == true#相手の事を知っている場合の二人称
           @target = "#{$msg.short_name($msg.t_target)}-girl" #仲間への二人称
           @target = "#{$msg.short_name($msg.t_target)}-girl" if $msg.age > 0 #年下仲間への二人称
-          @target = "Sister #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
+          @target = "Big sis #{$msg.short_name($msg.t_target)}" if $msg.age < 0 #年上仲間への二人称
           @target = "#{$msg.short_name($msg.t_target)}-girl" if @doppel #同属仲間への二人称
         else
           @target = "Sis" #仲間への二人称
           @target = "Sis" if $msg.age > 0 #年下仲間への二人称
-          @target = "Big Sister" if $msg.age < 0 #年上仲間への二人称
+          @target = "Big sister" if $msg.age < 0 #年上仲間への二人称
           @target = "Sis" if @doppel #同属仲間への二人称
         end
       end
@@ -87,12 +87,12 @@ class MsgGangcommander_A < MsgBase
           if @companion_cp == true
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}-girl" #相方への二人称
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}-girl" if $msg.age3 > 0 #年下相方への二人称
-            @cp_leader = "Sister #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
+            @cp_leader = "Big sis #{$msg.short_name($msg.coop_leader)}" if $msg.age3 < 0 #年上相方への二人称
             @cp_leader = "#{$msg.short_name($msg.coop_leader)}-girl" if @doppel_cp #同属相方への二人称
           else
             @cp_leader = "Sis" #相方への二人称
             @cp_leader = "Sis" if $msg.age3 > 0 #年下仲間への二人称
-            @cp_leader = "Big Sister" if $msg.age3 < 0 #年上仲間への二人称
+            @cp_leader = "Big sister" if $msg.age3 < 0 #年上仲間への二人称
             @cp_leader = "Sis" if @doppel_cp #同属仲間への二人称
           end
         end
