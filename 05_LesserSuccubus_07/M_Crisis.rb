be4f5b6e8bca63090976d4ee7336858250a0add5
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-11-06 13:17:34 +0900

    Update M_Crisis.rb

--- 05_LesserSuccubus_07/M_Crisis.rb
+++ 05_LesserSuccubus_07/M_Crisis.rb
@@ -45,7 +45,7 @@ when "User rebound harm"
   ms.push(tx)
 #主人公が自慰行為等で自らクライシス
 when "User onanism"
-  tx["tx1"] = "\"Whoa....\\H\n Wanna have a look....? \\H\""
+  tx["tx1"] = "\"Whoa....\\H\n You want me to have a look....? \\H\""
   #格納
   ms.push(tx)
 #----------------------------------------------------------------------------------------------------------------------
@@ -123,7 +123,7 @@ when "User rebound harm"
   ms.push(tx)
 #主人公が自慰行為等で自らクライシス
 when "User onanism"
-  tx["tx1"] = "\"Whoa....\\H\n Wanna have a look....? \\H\""
+  tx["tx1"] = "\"Whoa....\\H\n You want me to have a look....? \\H\""
   #格納
   ms.push(tx)
 #----------------------------------------------------------------------------------------------------------------------
@@ -193,7 +193,7 @@ when "User rebound harm"
   ms.push(tx)
 #主人公が自慰行為等で自らクライシス
 when "User onanism"
-  tx["tx1"] = "\"Whoa....\\H\n Wanna have a look....? \\H\""
+  tx["tx1"] = "\"Whoa....\\H\n You want me to have a look....? \\H\""
   #格納
   ms.push(tx)
 #----------------------------------------------------------------------------------------------------------------------
@@ -262,7 +262,7 @@ when "User rebound harm"
   ms.push(tx)
 #主人公が自慰行為等で自らクライシス
 when "User onanism"
-  tx["tx1"] = "\"Whoa....\\H\n Wanna have a look....? \\H\""
+  tx["tx1"] = "\"Whoa....\\H\n You want me to have a look....? \\H\""
   #格納
   ms.push(tx)
 #----------------------------------------------------------------------------------------------------------------------
