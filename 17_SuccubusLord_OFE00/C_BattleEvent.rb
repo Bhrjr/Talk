fd4b0db3fa974f8f2532814d9ea5da6a7389d185
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-11-13 14:12:59 +0900

    Update C_BattleEvent.rb

--- 17_SuccubusLord_OFE00/C_BattleEvent.rb
+++ 17_SuccubusLord_OFE00/C_BattleEvent.rb
@@ -17,7 +17,7 @@ class MsgSuccubuslord_B < MsgBase
                             #空ハッシュ挿入
                             tx={}
 #----------------------------------------------------------------------------------------------------------------------
-if $msg.tag == "戦闘開始"
+if $msg.tag == "Start the Battle"
                             #ムード格納(個別変更の場合は口上単位で修正する)
                             if $game_switches[85] == true
                               tx["md"] = "12" if $msg.talk_step == 1
@@ -38,38 +38,38 @@ if $msg.tag == "戦闘開始"
   if $game_switches[85] == true
     case $msg.talk_step
     when 0 #１番目
-      tx["tx1"] = "「#{target}と一夜を過ごせて嬉しいわ。\n　今夜はどのように愛してくれるの？\n　ふふっ、楽しみね\\H」"
+      tx["tx1"] = "\"I'm glad I'll get to spend the night with you, #{target}.\n How will you show me your love tonight?\n Ufufu, can't wait to see\\H\""
       #▼初ベッドイン
-      tx["tx1"] = "「ふふっ、待ってたわ。\n　今宵はどんな欲望でも受け入れてあげる、\n　#{target}のすべてを見せて……\\H」" if $msg.t_enemy.bedin_count == 0
+      tx["tx1"] = "\"Ufufu, I've been waiting for you.\n I'm willing to take in whatever lust you have tonight.\n Show me your everything, #{target}....\\H\"" if $msg.t_enemy.bedin_count == 0
     when 1 #２番目
-      tx["tx1"] = "「#{target}の求めることを、\n　#{myname}のカラダに教えてちょうだい。\n　きっとすべてに応じてみせるわ\\H」"
+      tx["tx1"] = "\"I want you to show me what\n you want to my body, #{target}.\n I'll make sure of it to answer it all\\H\""
       #▼初ベッドイン
-      tx["tx1"] = "「ここで逢うのは初めてね。\n　ありのままの#{target}の姿を\n　#{myname}に見せてちょうだい\\H」" if $msg.t_enemy.bedin_count == 0
+      tx["tx1"] = "\"It's my first time to get together with you here.\n I'd like you to show me\n your very whole being\\H\"" if $msg.t_enemy.bedin_count == 0
     when 2 #３番目
-      tx["tx1"] = "「すべて任せてくれても構わないわ。\n　最高の夜にしてあげる\\H」"
+      tx["tx1"] = "\"Feel free to leave everything to me.\n I'll do well to make this night the best\\H\""
       #▼初ベッドイン
-      tx["tx1"] = "「#{giggle_s}、#{target}は欲張りね。\n　ちゃんと全員のことを愛してくれるのかしら\\H」" if $msg.t_enemy.bedin_count == 0
+      tx["tx1"] = "\"#{giggle_s} Anyone told you how greedy you are, #{target}?\n I doubt you'll be able to show proper love to everyone\\H\"" if $msg.t_enemy.bedin_count == 0
     end
   #●空腹戦闘中
   elsif $game_switches[86] == true
     case $msg.talk_step
     when 0 #１番目
-    tx["tx1"] = "「空腹を耐えた分、\n　より#{target}が魅力的に見えるわ……\n　ふふっ、加減を間違えたらごめんなさい\\H」"
-      tx["tx1"] = "「こうして相対するのも久しぶりね……\\H\n　初めての時みたいに愛してみせてちょうだい\\H」\n#{speaker}の瞳は情欲に輝いている……！" if $msg.t_enemy.have_ability?("大切な人")
+    tx["tx1"] = "\"You'll get to see the hunger I endured\n in full charm....\nUfufu, sorry if I go too hard on you\\H\""
+      tx["tx1"] = "\"Been a long time since I took you on like this....\\H\n See to it that you show me love like how you did the first time\\H\"\n#{speaker}'s eyes are gleaming with lust....!" if $msg.t_enemy.have_ability?("Significant Other")
     when 1 #２番目
-      tx["tx1"] = "「はしたないかもしれないけれど、\n　お腹が空くのは夢魔だって同じよ。\n　ああ、#{target}の匂い、素敵……\\H」"
+      tx["tx1"] = "\"I don't know if this is shameful of me, but\n but this is how it is for all succubi once they're hungry.\n Aaah, you smell wonderful, #{target}....\\H\""
     when 2 #３番目
-      tx["tx1"] = "「#{giggle_s}、よりどりみどりね\\H\n　さあ、#{target}は\n　誰から愛してくれるのかしら\\H」"
+      tx["tx1"] = "\"#{giggle_s} Looks like you get to pick and choose\\H\n I just wonder who'll be the one\n to give you love, #{target}\\H\""
     end
   #●レア戦闘中(OFEの場合は[$game_switches[92] == true]、BOSSの場合は[$game_switches[91] == true]をそれぞれelsifでつけること)
   else
     case $msg.talk_step
     when 0 #１番目
-      tx["tx1"] = "「あら、迷子かしら？\n　……#{giggle_s}、そんなわけないわよね。\n　こんなところまで何のご用事かしら\\H」"
+      tx["tx1"] = "\"Oh my, a lost child, perhaps?\n ....#{giggle_s} That can't be.\n What could you want to have come this far? \\H\""
     when 1 #２番目
-      tx["tx1"] = "「逃がしてなんてあげないわ。\n　#{target}のこと、可愛がってあげる\\H」"
+      tx["tx1"] = "\"I'm not letting you get away.\n Instead, I'll treat you with love, #{target}\\H\""
     when 2 #３番目
-      tx["tx1"] = "「#{giggle_s}、かわいらしいお顔ね。\n　蕩けさせてあげたくなっちゃうわ……\\H」"
+      tx["tx1"] = "\"#{giggle_s} What a lovely face.\n Makes me just want to captivate it....\\H\""
     end
   end
   #格納
@@ -81,8 +81,8 @@ if $msg.tag == "戦闘開始"
 
 #----------------------------------------------------------------------------------------------------------------------
 #■逃走失敗口上
-elsif $msg.tag == "逃走失敗"
-  tx["tx1"] = "「だめよ、まだ満足できてないもの。\n　もちろん、#{target}だってそうでしょう\\H」"
+elsif $msg.tag == "Escape fail"
+  tx["tx1"] = "\"Nuh-uh, I'm not satisfied yet.\n Of course, I'm certain you're the same, #{target}\\H\""
   #----------------------------------------------------------------------------------------------------------------------
   #格納
   tx["md"] = "3"
@@ -94,7 +94,7 @@ elsif $msg.tag == "逃走失敗"
 
 #----------------------------------------------------------------------------------------------------------------------
 #■契約口上
-elsif $msg.tag == "契約"
+elsif $msg.tag == "Contract"
   #----------------------#
   #▼ステップで状況判断  #
   #----------------------#
@@ -102,32 +102,32 @@ elsif $msg.tag == "契約"
   #▼契約開始
   when 1 
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「#{target}のこと可愛がってあげようと思ったのに、\n　まさか#{myname}が可愛がられちゃうなんてね……\\H」"
+    tx["tx1"] = "\"I was going to treat you with love,\n but I was the one that got treated with love instead....\\H\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "「#{myname}、#{target}のことが気に入ったわ。\n　ねぇ、#{myname}のこと、傍に置いてくれないかしら？」"
+    tx["tx2"] = "\"#{myname}'ve taken a liking to you, #{target}.\n I wonder if you can let me be by your side?\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx3"] = "「求められればいつだってこのカラダを差し出すわ。\n　話し相手にもなるし、もちろん夜の相手だって歓迎よ。\n　……どうかしら？」"
+    tx["tx3"] = "\"I'll off up this body whenever you want.\n We'll get to share some talks, and what we do at nighttime goes without questin.\n ....What do you say?」"
     #----------------------------------------------------------------------------------------------------------------------
   #▼契約締結(即座に仲間に入る)
   when 2
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「ふふっ、ありがとう。\n　よろしくお願いするわね、#{target}\\H」"
+    tx["tx1"] = "\"Ufufu, thanks.\n We'll be partners from here on out, #{target}\\H\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "#{speaker}は嬉しそうな笑顔を浮かべ、\n#{master}の手を両手で包んだ。"
+    tx["tx2"] = "#{speaker} smiled happily and\n wrapped #{master}'s hand with both her hands."
     #----------------------------------------------------------------------------------------------------------------------
   #▼契約締結(ホームに送る)
   when 3
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「ふふっ、ありがとう。\n　用があれば呼んでちょうだい、いつでも待ってるわ\\H」"
+    tx["tx1"] = "\"Ufufu, thanks.\n Call me if you need something. I'll be waiting at all times\\H\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "#{master}の頬に軽く口付けて、\n#{speaker}は柔らかく微笑んでみせた。"
+    tx["tx2"] = "Pecking #{master} on the cheek, \n#{speaker} smiled softly."
     #----------------------------------------------------------------------------------------------------------------------
   #▼契約を行わない
   when 4
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「……そう、残念ね。\n　今日のところはおとなしく引き下がることにするわ」"
+    tx["tx1"] = "\"....I see, that's a shame.\n I suppose I'll pull out for today.\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "「気が変わったらいつでも会いに来てちょうだい。\n　#{myname}を振った#{target}のこと、\n　皆で熱烈に歓迎してあげる\\H」"
+    tx["tx2"] = "\"Come and see me again whenever you change your mind.\n Knowing that you've turned me down,\n I'll be sure to give you a passionate welcome with everyone\\H\""
     #----------------------------------------------------------------------------------------------------------------------
   end
   #格納
