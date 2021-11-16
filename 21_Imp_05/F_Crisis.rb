cd9a3683d346ba260a943989b03b0d4e9a192b6d
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-11-16 15:49:22 +0900

    Update F_Crisis.rb

--- 21_Imp_05/F_Crisis.rb
+++ 21_Imp_05/F_Crisis.rb
@@ -30,44 +30,44 @@ class MsgImp_A < MsgBase
 case $msg.tag
 #夢魔の攻めでパートナーがクライシス
 when "User solo"
-  tx["tx1"] = "「あはっ、気持ちいい？\n　#{myname}じょうずでしょ♪」"
+  tx["tx1"] = "\"Aha, does it feel good?\n #{myname}'m pretty good, right? ♪\""
   #格納
   ms.push(tx)
 #夢魔が既にクライシス、その夢魔から攻めを受けパートナーもクライシス
 when "Both users"
-  tx["tx1"] = "「#{pleasure_s}\n　#{target}も飛んじゃいそう……？\n　ね、#{myname}をぎゅーってしてくれる……？\\H」"
+  tx["tx1"] = "\"#{pleasure_s}\n You're about to bounce too, #{target}....?\n Can you hold me tight then....?\\H\""
   #格納
   ms.push(tx)
 #パートナーがホールド等のリバウンドで自らクライシス
 when "User rebound harm"
-  tx["tx1"] = "「あれれ～、もうおしまいかな～？\n　#{myname}は何もしてないんだけどな～？♪\n　#{giggle}」"
+  tx["tx1"] = "\"Huuh??? You're gonna finish already??\n But #{myname} wasn't doing anything♪\n　#{giggle}\""
   #格納
   ms.push(tx)
 #パートナーが自慰行為等で自らクライシス
 when "User onanism"
-  tx={"tx1" => "「わぁ……\\H\n　そんなふうにしたら気持ちいいんだぁ……\\H」","md" => "3"}
+  tx={"tx1" => "\"Wowie....\\H\n So it feels nice to do it like that, huh....\\H\"","md" => "3"}
   #格納
   ms.push(tx)
 #----------------------------------------------------------------------------------------------------------------------
 #■夢魔がクライシス状態になる
 #パートナーの攻めで夢魔がクライシス
 when "Enemy solo"
-  tx["tx1"] = "「#{target}、#{myname}ね……。\n　なんだか、からだがふわふわしてきた……\\H」"
+  tx["tx1"] = "\"#{target}, I....\n My body feels kinda light and fluffy....\\H\""
   #格納
   ms.push(tx)
 #パートナーが既にクライシス、そのパートナーから攻めを受け夢魔もクライシス
 when "Both foes"
-  tx["tx1"] = "「#{target}、#{myname}をぎゅってして……！\n　なんだか飛んでっちゃいそう……！\\H」"
+  tx["tx1"] = "\"Hold me tight, #{target}....!\n I feel like I'm gonna bounce....! \\H\""
   #格納
   ms.push(tx)
 #夢魔がホールド等のリバウンドで自らクライシス
 when "Enemy rebound harm"
-  tx["tx1"] = "「#{target}、#{myname}ね……。\n　なんだか、からだがふわふわしてきた……\\H」"
+  tx["tx1"] = "\"#{target}, I....\n My body feels kinda light and fluffy....\\H\""
   #格納
   ms.push(tx)
 #夢魔が自慰行為等で自らクライシス
 when "Enemy onanism"
-  tx["tx1"] = "「あぁん……気持ちいいよぉ……\\H\n　#{myname}のここ、触ってぇ……\\H」"
+  tx["tx1"] = "\"Anngh....that feels so good....\\H\n Touch me down here....\\H\""
   #格納
   ms.push(tx)
 #夢魔が味方夢魔から攻められてクライシス
@@ -102,7 +102,7 @@ class MsgImp_A < MsgBase
 
 #====================================================================================================================
 #◆【大事な人】状態◆
-if $msg.t_enemy.have_ability?("大切な人")
+if $msg.t_enemy.have_ability?("Significant Other")
 #====================================================================================================================
 #■パートナーがクライシス状態になる
 case $msg.tag
@@ -172,7 +172,7 @@ end #case $msg.tag
 
 #====================================================================================================================
 #◆【寵愛】状態◆
-elsif $msg.t_enemy.have_ability?("寵愛")
+elsif $msg.t_enemy.have_ability?("Affection")
 #====================================================================================================================
 #■パートナーがクライシス状態になる
 case $msg.tag
