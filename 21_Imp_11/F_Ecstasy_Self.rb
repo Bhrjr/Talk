caf26db71b5d8fbb50ef6d7fb125029e5783fe45
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-11-19 13:41:23 +0900

    Update F_Ecstasy_Self.rb

--- 21_Imp_11/F_Ecstasy_Self.rb
+++ 21_Imp_11/F_Ecstasy_Self.rb
@@ -25,7 +25,7 @@ class MsgImp_A < MsgBase
   #==================================================================================================================
   #●基礎口上を設定(未設定項目or空白の口上が呼ばれた時に設定され、それ以外では表示されない)
   #==================================================================================================================
-  tx["tx1"] = "「#{pleasure_l}\n　#{pleasure_cr}」"
+  tx["tx1"] = "\"#{pleasure_l}\n #{pleasure_cr}\""
   #格納
   ms.push(tx)
   #==================================================================================================================
@@ -36,19 +36,19 @@ class MsgImp_A < MsgBase
 case $msg.talk_step
 #★汎用★==============================================================================================================
 when 1 #初撃
-  tx["tx1"] = "「#{pleasure_l}\n　きもちいい……きもちいいよぉ……\\H」"
+  tx["tx1"] = "\"#{pleasure_l}\n That feels good....so good....\\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{pleasure_l}\n　#{myname}、からだがふわふわしてきた……\\H」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{pleasure_l}\n #{myname}, my body feels light and fluffy now....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 9 #とどめ
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」"
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 10 #余韻
-  tx["tx1"] = "「あはぁ……\\H\n　あははっ、なんだかふらふらするぅ……\\H」"
-  tx["tx2"] = "「ねぇ、今のもっともっとやって\\H\n　ね、#{target}、おねがぁい\\H」"
+  tx["tx1"] = "\"Aha....\\H\n Ahaha, I feel kinda dizzy....\\H\""
+  tx["tx2"] = "\"Hey, keep doing what you did just now\\H\n Pretty please, #{target}? \\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「あはぁ……\\H\n　気持ちよかったぁ……\\H」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
-  tx["tx2"] = "「……ねぇ、今のもっかいやって？\n　#{myname}、今の好き\\H\n　ね、いいでしょ？」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx1"] = "\"Aha....\\H\n That felt nice....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx2"] = "\"....Hey, can you do that just now again?\n I like it\\H\n Hey, isn't it fine?\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
 end
 #格納
 ms.push(tx)
@@ -132,7 +132,7 @@ class MsgImp_A < MsgBase
   #==================================================================================================================
   #●基礎口上を設定(未設定項目or空白の口上が呼ばれた時に設定され、それ以外では表示されない)
   #==================================================================================================================
-  tx["tx1"] = "「#{pleasure_l}\n　#{pleasure_cr}」"
+  tx["tx1"] = "\"#{pleasure_l}\n #{pleasure_cr}\""
   #格納
   ms.push(tx)
   #==================================================================================================================
@@ -149,24 +149,24 @@ class MsgImp_A < MsgBase
 
 #====================================================================================================================
 #◆【大事な人】状態◆
-if $msg.t_enemy.have_ability?("大切な人")
+if $msg.t_enemy.have_ability?("Significant Other")
 #====================================================================================================================
 case $msg.talk_step
 #★汎用★==============================================================================================================
 when 1 #初撃
-  tx["tx1"] = "「#{pleasure_l}\n　きもちいい……きもちいいよぉ……\\H」"
+  tx["tx1"] = "\"#{pleasure_l}\n That feels good....so good....\\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{pleasure_l}\n　#{myname}、からだがふわふわしてきた……\\H」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{pleasure_l}\n #{myname}, my body feels light and fluffy now....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 9 #とどめ
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」"
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 10 #余韻
-  tx["tx1"] = "「あはぁ……\\H\n　あははっ、なんだかふらふらするぅ……\\H」"
-  tx["tx2"] = "「ねぇ、今のもっともっとやって\\H\n　ね、#{target}、おねがぁい\\H」"
+  tx["tx1"] = "\"Aha....\\H\n Ahaha, I feel kinda dizzy....\\H\""
+  tx["tx2"] = "\"Hey, keep doing what you did just now\\H\n Pretty please, #{target}? \\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「あはぁ……\\H\n　気持ちよかったぁ……\\H」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
-  tx["tx2"] = "「……ねぇ、今のもっかいやって？\n　#{myname}、今の好き\\H\n　ね、いいでしょ？」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx1"] = "\"Aha....\\H\n That felt nice....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx2"] = "\"....Hey, can you do that just now again?\n I like it\\H\n Hey, isn't it fine?\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
 end
 #格納
 ms.push(tx)
@@ -238,24 +238,24 @@ ms.push(tx)
 
 #====================================================================================================================
 #◆【寵愛】状態◆
-elsif $msg.t_enemy.have_ability?("寵愛")
+elsif $msg.t_enemy.have_ability?("Affection")
 #====================================================================================================================
 case $msg.talk_step
 #★汎用★==============================================================================================================
 when 1 #初撃
-  tx["tx1"] = "「#{pleasure_l}\n　きもちいい……きもちいいよぉ……\\H」"
+  tx["tx1"] = "\"#{pleasure_l}\n That feels good....so good....\\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{pleasure_l}\n　#{myname}、からだがふわふわしてきた……\\H」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{pleasure_l}\n #{myname}, my body feels light and fluffy now....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 9 #とどめ
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」"
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 10 #余韻
-  tx["tx1"] = "「あはぁ……\\H\n　あははっ、なんだかふらふらするぅ……\\H」"
-  tx["tx2"] = "「ねぇ、今のもっともっとやって\\H\n　ね、#{target}、おねがぁい\\H」"
+  tx["tx1"] = "\"Aha....\\H\n Ahaha, I feel kinda dizzy....\\H\""
+  tx["tx2"] = "\"Hey, keep doing what you did just now\\H\n Pretty please, #{target}? \\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「あはぁ……\\H\n　気持ちよかったぁ……\\H」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
-  tx["tx2"] = "「……ねぇ、今のもっかいやって？\n　#{myname}、今の好き\\H\n　ね、いいでしょ？」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx1"] = "\"Aha....\\H\n That felt nice....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx2"] = "\"....Hey, can you do that just now again?\n I like it\\H\n Hey, isn't it fine?\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
 end
 #格納
 ms.push(tx)
@@ -331,19 +331,19 @@ else
 case $msg.talk_step
 #★汎用★==============================================================================================================
 when 1 #初撃
-  tx["tx1"] = "「#{pleasure_l}\n　きもちいい……きもちいいよぉ……\\H」"
+  tx["tx1"] = "\"#{pleasure_l}\n That feels good....so good....\\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{pleasure_l}\n　#{myname}、からだがふわふわしてきた……\\H」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{pleasure_l}\n #{myname}, my body feels light and fluffy now....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 9 #とどめ
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」"
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「#{target}、\n　おねがい、ぎゅーってしてて……！\n　#{pleasure_cr}」" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
+  tx["tx1"] = "\"#{target},\n please, hold me tight....!\n　#{pleasure_cr}\"" if $msg.t_enemy.ecstasy_count.size <= 0 #絶頂初回
 when 10 #余韻
-  tx["tx1"] = "「あはぁ……\\H\n　あははっ、なんだかふらふらするぅ……\\H」"
-  tx["tx2"] = "「ねぇ、今のもっともっとやって\\H\n　ね、#{target}、おねがぁい\\H」"
+  tx["tx1"] = "\"Aha....\\H\n Ahaha, I feel kinda dizzy....\\H\""
+  tx["tx2"] = "\"Hey, keep doing what you did just now\\H\n Pretty please, #{target}? \\H\""
 #  tx["tx1"] = "" if $msg.t_enemy.ecstasy_emotion == "怒" #パターンB
-  tx["tx1"] = "「あはぁ……\\H\n　気持ちよかったぁ……\\H」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
-  tx["tx2"] = "「……ねぇ、今のもっかいやって？\n　#{myname}、今の好き\\H\n　ね、いいでしょ？」" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx1"] = "\"Aha....\\H\n That felt nice....\\H\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
+  tx["tx2"] = "\"....Hey, can you do that just now again?\n I like it\\H\n Hey, isn't it fine?\"" if $msg.t_enemy.ecstasy_count.size <= 1 #絶頂初回
 end
 #格納
 ms.push(tx)
