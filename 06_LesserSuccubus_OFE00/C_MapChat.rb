7340d087db854b90592c5b9947c194c8174f8d10
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-11-19 20:19:46 +0900

    Update C_MapChat.rb

--- 06_LesserSuccubus_OFE00/C_MapChat.rb
+++ 06_LesserSuccubus_OFE00/C_MapChat.rb
@@ -56,22 +56,22 @@ elsif $msg.t_enemy.spp <= 20 or $msg.t_enemy.hpp <= 20
   #◆【大事な人】状態◆
   if $msg.t_enemy.have_ability?("Significant Other")
   #====================================================================================================================
-    tx["tx1"] = "\"H-Hey, #{target}...let's go home already....\n I'm beat....\""
-    tx["tx4"] = "#{speaker} pulled the hem of his clothes....\nLooks like she's worn out...." + "TALKTEXT"
+    tx["tx1"] = "\"H-Hey, #{target}...let's go home already....\n I'm ready to drop....\""
+    tx["tx4"] = "#{speaker} pulled the hem of his clothes....\nLooks like she's quite worn out...." + "TALKTEXT"
 
   #====================================================================================================================
   #◆【寵愛】状態◆
   elsif $msg.t_enemy.have_ability?("Affection")
   #====================================================================================================================
-    tx["tx1"] = "\"H-Hey, #{target}...let's go home already....\n I'm beat....\""
-    tx["tx4"] = "#{speaker} pulled the hem of his clothes....\nLooks like she's worn out...." + "TALKTEXT"
+    tx["tx1"] = "\"H-Hey, #{target}...let's go home already....\n I'm ready to drop....\""
+    tx["tx4"] = "#{speaker} pulled the hem of his clothes....\nLooks like she's quite worn out...." + "TALKTEXT"
 
   #====================================================================================================================
   #◆通常状態(【寵愛】【大切な人】どちらも持っていない場合)◆
   else
   #====================================================================================================================
-    tx["tx1"] = "\"H-Hey, #{target}...let's go home already....\n I'm beat....\""
-    tx["tx4"] = "#{speaker} pulled the hem of his clothes....\nLooks like she's worn out...." + "TALKTEXT"
+    tx["tx1"] = "\"H-Hey, #{target}...let's go home already....\n I'm ready to drop....\""
+    tx["tx4"] = "#{speaker} pulled the hem of his clothes....\nLooks like she's quite worn out...." + "TALKTEXT"
 
   end
   #格納
