070cb904003f3f8c64f5d87e1e5e336b6997e97b
Bhrjr <89809317+Bhrjr@users.noreply.github.com>
2021-11-25 05:46:11 +0900

    Update C_BattleEvent.rb

--- 26_Devil_11/C_BattleEvent.rb
+++ 26_Devil_11/C_BattleEvent.rb
@@ -16,7 +16,7 @@ class MsgDevil_A < MsgBase
                             #空ハッシュ挿入
                             tx={}
 #----------------------------------------------------------------------------------------------------------------------
-if $msg.tag == "戦闘開始"
+if $msg.tag == "Start the Battle"
                             #ムード格納(個別変更の場合は口上単位で修正する)
                             if $game_switches[85] == true
                               tx["md"] = "12" if $msg.talk_step == 1
@@ -37,40 +37,40 @@ if $msg.tag == "戦闘開始"
   if $game_switches[85] == true
     case $msg.talk_step
     when 0 #１番目
-      tx["tx1"] = "「……え、もう始めるのかい？\n　あ、いやまあ#{myname}は構わないけどな」"
+      tx["tx1"] = "\"....Eh, we're starting already?\n Oh, well #{myname} don't mind that.\""
       #▼初ベッドイン
-      tx["tx1"] = "「……何だよ、そんなにじろじろ見るなよ。\n　……ばっ……！別に緊張してるわけじゃないって！」" if $msg.t_enemy.bedin_count == 0
-      tx["tx2"] = "「この～……覚悟しろよ？\n　すぐに参ったって言わせてやるぜ！」" if $msg.t_enemy.bedin_count == 0
+      tx["tx1"] = "\"....Wha, quit staring at me like that.\n ....Bah....! I'm not nervous or anything!\"" if $msg.t_enemy.bedin_count == 0
+      tx["tx2"] = "\"Why you....you better be prepared.\n I'm gonna make you say you lost ASAP!\"" if $msg.t_enemy.bedin_count == 0
     when 1 #２番目
-      tx["tx1"] = "「さぁて、どんな風に攻めて欲しい？\n　希望があればお応えするぜ？\\H」"
+      tx["tx1"] = "\"Now how do you want me to attack you?\n If there's one you want, I'll do it for you\\H\""
       #▼初ベッドイン
-      tx["tx1"] = "「あ～……その、何だ。\n　これから、するんだよな？」" if $msg.t_enemy.bedin_count == 0
-      tx["tx2"] = "「……ええい、契約しちまったんだし仕方ねえ。\n　煮るなり焼くなり好きにしろってんだ」" if $msg.t_enemy.bedin_count == 0
+      tx["tx1"] = "\"Aaah....what's with that?\n We're doing it from now on, yeah?\"" if $msg.t_enemy.bedin_count == 0
+      tx["tx2"] = "\"....Gah, that's what I get for forming a contract with you.\n Just do whatever you want with me.\"" if $msg.t_enemy.bedin_count == 0
     when 2 #３番目
-      tx["tx1"] = "「#{target}も頑張るねぇ、３人同時に相手か。\n　ま、ここはイイ所を見せてもらおうかな\\H」"
+      tx["tx1"] = "\"Facing 3 people at once tells me how hardworking you are.\n Maybe I should show you something nice here\\H\""
       #▼初ベッドイン
-      tx["tx1"] = "「んじゃ、ま……お手柔らかに頼むぜ？\n　正直、こういう雰囲気でするのは初めてでな」" if $msg.t_enemy.bedin_count == 0
-      tx["tx2"] = "「……こらそこ！\n　変な笑顔を浮かべるんじゃない！」" if $msg.t_enemy.bedin_count == 0
+      tx["tx1"] = "\"So....can I ask you to go easy on me?\n In all honesty, I've never done it before with the modd like this.\"" if $msg.t_enemy.bedin_count == 0
+      tx["tx2"] = "\"....You there!\n Don't you make that weird smile at me!\"" if $msg.t_enemy.bedin_count == 0
     end
   #●空腹戦闘中
   elsif $game_switches[86] == true
     case $msg.talk_step
     when 0 #１番目
-      tx["tx1"] = "「#{target}もなぁ……。\n　もっと素直にしてれば、\n　こんな目に遭わなくて済んだんだぜ？」"
+      tx["tx1"] = "\"Damn, #{target}....\n If you just stay obedent,\n you wouldn't hafta go through any of this, you hear?\""
     when 1 #２番目
-      tx["tx1"] = "「#{giggle}\n　さぁて、大人しくしてなよ……？」"
+      tx["tx1"] = "\"#{giggle}\n Just keep still, alright....?\""
     when 2 #３番目
-      tx["tx1"] = "「んじゃ、覚悟を決めてもらおうかね？\n　#{giggle}」"
+      tx["tx1"] = "\"I really hope you're prepared here.\n #{giggle}\""
     end
   #●レア戦闘中(OFEの場合は[$game_switches[92] == true]、BOSSの場合は[$game_switches[91] == true]をそれぞれelsifでつけること)
   else
     case $msg.talk_step
     when 0 #１番目
-      tx["tx1"] = "「おっと……珍しいな。\n　人間の男……ってか子供か？\n　面白い、丁度退屈してたんだ」"
+      tx["tx1"] = "\"Whoa there....what a rare sight.\n A human male....or is it a kid?\n How interesting, I was just getting bored.\""
     when 1 #２番目
-      tx["tx1"] = "「ふぅん……ま、身体はそこそこみたいだが？\n　さて、どう楽しませてもらえるかな？\\H」"
+      tx["tx1"] = "\"Hmph....physiques-wise, he looks fairly alright.\n Now how'll you manage to entertain me, I wonder? \\H\""
     when 2 #３番目
-      tx["tx1"] = "「ま、あまり期待はしない方がいいな。\n　せいぜい頑張ってもらおうか\\H」"
+      tx["tx1"] = "\"Well, it's better that I don't get my hopes up too much.\n What say I try my very best here? \\H\""
     end
   end
   #格納
@@ -82,8 +82,8 @@ if $msg.tag == "戦闘開始"
 
 #----------------------------------------------------------------------------------------------------------------------
 #■逃走失敗口上
-elsif $msg.tag == "逃走失敗"
-  tx["tx1"] = "「どこへ行くんだい#{target}？\n　そんなに#{myname}が恐ろしいのかい？」"
+elsif $msg.tag == "Escape fail"
+  tx["tx1"] = "\"Where you going, #{target}?\n You're that scared of me?\""
   #----------------------------------------------------------------------------------------------------------------------
   #格納
   tx["md"] = "3"
@@ -95,7 +95,7 @@ elsif $msg.tag == "逃走失敗"
 
 #----------------------------------------------------------------------------------------------------------------------
 #■契約口上
-elsif $msg.tag == "契約"
+elsif $msg.tag == "Contract"
   #----------------------#
   #▼ステップで状況判断  #
   #----------------------#
@@ -103,34 +103,34 @@ elsif $msg.tag == "契約"
   #▼契約開始
   when 1 
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「ぐっ……！\n　くそ、負けちまったか……。\n　大抵の奴は#{myname}を見て縮こまるってのに……」"
+    tx["tx1"] = "\"Gah....!\n Crap, I lost, eh....\n Most guys would just cower at the sight of me....\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "「ええい、負けは負けだ！\n　犯し嬲るなり、契約するなり好きにしてくれ」"
+    tx["tx2"] = "\"Gaah, I lost, dammit!\n Fuck me, mock me, form a contract with me; do whatever you want.\""
     #----------------------------------------------------------------------------------------------------------------------
   #▼契約締結(即座に仲間に入る)
   when 2
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「……え、契約……？\n　って、本気かい？\n　あ、いや、別に嫌って訳じゃないけどさ……」"
+    tx["tx1"] = "\"....Eh, a contract....?\n You serious 'bout that?\n Ah, well, not like I'd hate that or anything, really....\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "「……あー、その、こういうの初めてでな。\n　えーと、今後とも、よろしく……。\n　……って、これでいいのか？」"
+    tx["tx2"] = "\"....Ah-, well, that never happened before.\n Uhhh, nice to meet you....\n ....Is that good enough?\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx3"] = "「……うわ、なんか急にドキドキしてきたぞ！？\n　ええい、こっち見るなっ！」"
+    tx["tx3"] = "\"....Wow, my heart began to race all of a sudden!?\n Gaah, don't look here!\""
     #----------------------------------------------------------------------------------------------------------------------
   #▼契約締結(ホームに送る)
   when 3
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「……え、契約……？\n　って、本気かい？\n　あ、いや、別に嫌って訳じゃないけどさ……」"
+    tx["tx1"] = "\"....Eh, a contract....?\n You serious 'bout that?\n Ah, well, not like I'd hate that or anything, really....\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "「あー……とりあえずお仲間は一杯か。\n　んじゃ、#{target}の家に邪魔すればいいんだな」"
+    tx["tx2"] = "\"Ah....I see you're full with allies for now.\n Then I just need to make my way to your home.\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx3"] = "「……後悔、するなよな？\n　ふふ、とんだ物好きがいたもんだね……\\H」"
+    tx["tx3"] = "\"....Better not regret it, you hear?\n Hehe, so there are strange guys like that....\\H\""
     #----------------------------------------------------------------------------------------------------------------------
   #▼契約を行わない
   when 4
     #----------------------------------------------------------------------------------------------------------------------
-    tx["tx1"] = "「何だよ、何もしないって？\n　物好きな奴だな、#{target}」"
+    tx["tx1"] = "\"Wha, you're not gonna do anything to me?\n You're a strange one, #{target}.\""
     # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-    tx["tx2"] = "「次に会ったら、今度は本気を出してやるよ。\n　……本当だからな、覚悟してろよ？」"
+    tx["tx2"] = "\"When I see you again, I'm going all out.\n ....I'm not kidding, so prepare yourself, yeah?\""
     #----------------------------------------------------------------------------------------------------------------------
   end
   #格納
